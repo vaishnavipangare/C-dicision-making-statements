7c6e2f7bc7817d18e7ec6d65638da730e8d9f892
vaishnavipangare <vaishnavi.pangare.btech2024@sitpune.edu.in>
2025-07-30 17:44:01 -1200

    Create Finding whether a number is odd or even.cpp

---
+++ Finding whether a number is odd or even.cpp
@@ -0,0 +1,28 @@
+// EXPERIMENT-5
+// Vaishnavi pangare
+// 25070123501
+// ENTC A3
+//Problem Statement: Write a program in C++ to find whether a given number is even or odd.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num;
+  cout<<"Enter the number:"<<endl;
+  cin>>num;
+  if(num%2==0){
+      cout<<"The given number is even number."<<endl;
+  }
+  else{
+      cout<<"The given number is odd number."<<endl;
+  }
+    return 0;
+}
+
+
+/*Output
+Enter the number:
+78
+The given number is even number.
+*/
